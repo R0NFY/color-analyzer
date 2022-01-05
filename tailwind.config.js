d53d6b7d47327879739bfbdc2006f2655518aa2f
r0nfy <r0nfymain@gmail.com>
2022-01-05 14:30:08 +0700

    welcome page before tweaks

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.vue"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+      },
+      colors: {
+        dark: "#191919",
+      },
+      backgroundImage: {
+        pattern: "url(./assets/Taieri.svg)",
+      },
+    },
+  },
+  plugins: [],
+};
